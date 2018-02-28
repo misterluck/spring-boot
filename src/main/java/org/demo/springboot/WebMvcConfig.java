a339100f34d7d87c11d1609b8d75188465b16ede
misterluck <zl876170926@sina.com>
2018-02-28 17:18:46 +0800

    Initial commit

---
+++ src/main/java/org/demo/springboot/WebMvcConfig.java
@@ -0,0 +1,8 @@
+package org.demo.springboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+}
---
+++ src/main/java/org/demo/springboot/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package org.demo.springboot;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSocketConfig {
+}
