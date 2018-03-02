178cd48fe7bc675b9b60c56837adf1caae6530e8
misterluck <zl876170926@sina.com>
2018-03-02 15:56:49 +0800

    Initial commit

--- src/main/java/org/demo/springboot/bootstrap/WebSocketStompConfig.java
+++ src/main/java/org/demo/springboot/bootstrap/WebSocketStompConfig.java
@@ -2,6 +2,7 @@ package org.demo.springboot.bootstrap;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.*;
 
 /**
@@ -11,10 +12,21 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 public class WebSocketStompConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+    /**
+     * 该类继承AbstractWebSocketMessageBrokerConfigurer时注册websocket端点
+     */
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompWebSocket").withSockJS();
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/stompWebSocket").withSockJS();
     }
+
+    /**
+     * 该类继承AbstractSessionWebSocketMessageBrokerConfigurer时注册websocket端点
+     */
+    /*@Override
+    protected void configureStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/stompWebSocket").withSockJS();
+    }*/
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
